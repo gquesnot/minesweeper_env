aface53312dc093e6c4ecef528baae4e8f8a5c12
gabriel quesnot <gaqu1994@gmail.com>
2022-12-14 01:37:02 +0100

    reformat + requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import gym
 import minesweeper_env
 
+
 def main():
-    env = gym.make('minesweeper_env/Minesweeper-v0',render_mode="human", width= 10, height= 10, num_mines= 10)
+    env = gym.make('minesweeper_env/Minesweeper-v0', render_mode="human", width=10, height=10, num_mines=10)
     observation, info = env.reset(seed=42)
 
     for _ in range(1000):
